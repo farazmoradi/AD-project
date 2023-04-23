3ef2840c83d1fc4c7fa17d8216cec049210a3f06
farazmoradi <62336594+farazmoradi@users.noreply.github.com>
2023-04-23 18:47:42 -0400

    Delete run_preprocessing.m

--- run_preprocessing.m
+++
@@ -1,7 +0,0 @@
-clear all
-clc
-rootdir = 'D:\Projects\SpikeSorting\data';
-filelist = dir(fullfile(rootdir, '\**\*.edf')); 
-for n=1:length(filelist)
-   preprocessing(filelist(n).folder,filelist(n).name)
-end
